79bc69c00c1dea0ac4055b4f8ceb5d9c3642b9f3
taceroc <taceroc@unal.edu.co>
2019-09-13 11:43:51 -0500

    Hace la serie armonica para numeros del 1 al 1000, lo imprime en dos columnas, la primera es N y la segunda es la suma hasta ese N. Crea archivo txt con datos para realizar grafica

---
+++ 2.sumaarmonica.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cstdio>
+#include<fstream>
+
+int main(void)
+{
+  std::ofstream fout("sumarmonica1.txt");
+  double term = 0; double sum=0;
+  for(int N = 1.0; N < 1001; ++N){
+    term  = 1.0/N;
+    sum += term;
+          
+    std::printf("%6d %20.10f\n",N,sum);
+    fout<< N << "\t" << sum << "\n";
+  }
+  
+  return 0;
+}
